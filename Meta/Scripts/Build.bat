d2409c6b38994ef09449812ee8ec7975f3ca13bc
David Bos <david.bos@outlook.com>
2024-12-23 20:32:32 +0100

    ENV Init - stage8

--- Meta/Scripts/Build.bat
+++ Meta/Scripts/Build.bat
@@ -9,11 +9,11 @@ docker image inspect %DOCKER_IMAGE% >nul 2>&1
 if %ERRORLEVEL% neq 0 (
     echo Docker image "%DOCKER_IMAGE%" not found.
     echo Building it...
-    docker build -t %DOCKER_IMAGE% ../Enviroment
+    docker build -t %DOCKER_IMAGE% ../Environment
 )
 
 REM Set CARGO_TARGET_DIR inside Docker and build the kernel
-docker run --rm -v "%cd%\..\..:/build" -e CARGO_TARGET_DIR=/build/Meta/Target/Rust %DOCKER_IMAGE% bash -c "cd /build && cargo build --target x86_64-unknown-none --release"
+docker run --rm -v "%cd%\..\..:/build" -e CARGO_TARGET_DIR=/build/Meta/Target/Rust %DOCKER_IMAGE% bash -c "cd /build && Meta/Environment/linker.ld && cargo clean && cargo build --verbose --target x86_64-unknown-none --release"
 if %ERRORLEVEL% neq 0 (
     echo Build process failed inside Docker. Exiting...
     exit /b %ERRORLEVEL%
@@ -23,7 +23,7 @@ echo 'Build finished'
 REM Move the kernel to the ISO directory and create the bootable ISO
 echo Moving kernel and creating bootable ISO...
 docker run --rm -v "%cd%\..\..:/build" %DOCKER_IMAGE% bash -c "cd /build && mkdir -p Meta/Target/iso/boot/grub"
-docker run --rm -v "%cd%\..\..:/build" %DOCKER_IMAGE% bash -c "cp Meta/Enviroment/grub.cfg Meta/Target/iso/boot/grub/grub.cfg"
+docker run --rm -v "%cd%\..\..:/build" %DOCKER_IMAGE% bash -c "cp Meta/Environment/grub.cfg Meta/Target/iso/boot/grub/grub.cfg"
 docker run --rm -v "%cd%\..\..:/build" %DOCKER_IMAGE% bash -c "rm /build/Meta/Target/iso/boot/dnos-kernel.elf"
 docker run --rm -v "%cd%\..\..:/build" %DOCKER_IMAGE% bash -c "objdump -h Meta/Target/Rust/x86_64-unknown-none/release/dnos"
 docker run --rm -v "%cd%\..\..:/build" %DOCKER_IMAGE% bash -c "mv /build/Meta/Target/Rust/x86_64-unknown-none/release/dnos /build/Meta/Target/iso/boot/dnos-kernel.elf"
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,25 @@
 
 use core::panic::PanicInfo;
 
+// Multiboot header
+#[repr(C, packed)]
+struct MultibootHeader {
+    magic: u32,
+    flags: u32,
+    checksum: u32,
+}
+
+const MULTIBOOT_MAGIC: u32 = 0x1BADB002;
+const MULTIBOOT_FLAGS: u32 = 0b00000000000000000000000000000010;
+const MULTIBOOT_CHECKSUM: u32 = !(MULTIBOOT_MAGIC + MULTIBOOT_FLAGS);
+
+#[link_section = ".multiboot"]
+static MULTIBOOT_HEADER: MultibootHeader = MultibootHeader {
+    magic: MULTIBOOT_MAGIC,
+    flags: MULTIBOOT_FLAGS,
+    checksum: MULTIBOOT_CHECKSUM,
+};
+
 /// Panic handler: defines what happens when a panic occurs
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -12,5 +31,6 @@ fn panic(_info: &PanicInfo) -> ! {
 /// Kernel entry point: _start
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    // Your kernel code starts here
     loop {}
 }
